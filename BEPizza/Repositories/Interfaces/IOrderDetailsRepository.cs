f3e2688ed03d068861ec629068f69ce10097858a
cuongsuhuy <cuong.su@kyanon.digital>
2023-10-02 23:23:48 +0700

    Optimize Structure

---
+++ BEPizza/Repositories/Interfaces/IOrderDetailsRepository.cs
@@ -0,0 +1,13 @@
+﻿using BEPizza.Models;
+
+namespace BEPizza.Repositories.Interfaces
+{
+    public interface IOrderDetailsRepository
+    {
+        IEnumerable<OrderDetails> GetAllOrderDetails();
+        OrderDetails GetOrderDetailsById(int id);
+        void AddOrderDetails(OrderDetails orderDetails);
+        void UpdateOrderDetails(OrderDetails orderDetails);
+        void DeleteOrderDetails(int id);
+    }
+}
